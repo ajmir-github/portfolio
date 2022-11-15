a6a69e5fa4cb4d741ecee320f0202cbf4e6e4563
Ajmir Raziqi <ajmir.ng3@gmail.com>
2022-11-15 04:09:58 -0800

    js codes impoved

--- js/script.js
+++ js/script.js
@@ -1,49 +1,26 @@
-// Defautl Namings
-const classNameObserved = ".observe";
-const onShowAttributeName = "classShow";
-const onHideAttributeName = "classHide";
-const precentageOfVisiblity = 0.1;
-
 // Funcs
-function toggleTheClasses(element, visible) {
-  const classShow = element.attr(onShowAttributeName);
-  const classHide = element.attr(onHideAttributeName);
-  if(visible){
-    element.removeClass(classHide);
-    element.addClass(classShow);
+function toggle(element, visible) {
+  const show = element.attr("class-onShow");
+  const hide = element.attr("class-onHide");
+  if (visible) {
+    element.removeClass(hide);
+    element.addClass(show);
   } else {
-    element.removeClass(classShow);
-    element.addClass(classHide);
+    element.removeClass(show);
+    element.addClass(hide);
   }
 }
 
 // Main Object
-const observer = new IntersectionObserver(entries=>{
-  entries
-    .forEach(e => toggleTheClasses($(e.target), e.isIntersecting))
-}, {
-  threshold:precentageOfVisiblity
-});
-
-// Classes to obsever
-function observeTheClasses(className) {
-  // document
-  // .querySelectorAll(className)
-  // .forEach(element => observer.observe(element));
-  
-  // Jquery
-  for (const element of $(className))
-    observer.observe(element);
-}
-
-
+const observer = new IntersectionObserver(
+  (e) => e.forEach((e) => toggle($(e.target), e.isIntersecting)),
+  { threshold: 0.1 }
+);
 
 // On loaded
-$(()=>{
+$(() => {
   // activate the observer
-  observeTheClasses(classNameObserved);
-  
-  setTimeout(()=>{
-    $("#page-loading").remove();
-  }, 2000);
-})
+  for (const element of $(".observe")) observer.observe(element);
+  // Remove the page loading element
+  setTimeout(() => $("#page-loading").remove(), 2000);
+});
